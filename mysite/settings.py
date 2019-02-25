ae03f196a8afbf7e6c44f0f0c68417b1db7cd3ef
Samuel P Holmes <samuel.holmes@cgius.com>
2019-02-25 15:09:29 -0600

    txt files

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,4 +124,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 #django_heroku.settings(locals())
