22901876063b432f016aba2356286fe6b095c834
Samuel P Holmes <samuel.holmes@cgius.com>
2019-02-26 12:09:10 -0600

    mm

--- polls/urls.py
+++ polls/urls.py
@@ -4,15 +4,9 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-
-    #ex: /1/
     path('<int:employee_id>/', views.detailEmployee, name='detailEmployee'), 
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    
-    # ex: /polls/5/results/
+    path('<string:question_text>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
     path('addEmployee/', views.addEmployee, name='addEmployee'),
 ]
