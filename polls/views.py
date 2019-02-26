3888a9a9b6c97856f9337f7cd79e065f44660750
Samuel P Holmes <samuel.holmes@cgius.com>
2019-02-26 08:43:12 -0600

    edited index

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 def detail(request, question_id):
     try:
-        question = QUestion.objects.get(pk=question_id)
+        question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', { 'question' : question })
