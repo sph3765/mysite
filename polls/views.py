5b7048f6a7befc2040147fa6523a95e271ab730a
Samuel P Holmes <samuel.holmes@cgius.com>
2019-02-26 08:46:37 -0600

    edited index

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, Http404
 from .models import Question 
 from django.template import loader 
-from django.shortcuts import render 
+from django.shortcuts import render, get_object_or_404 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,11 +12,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', { 'question' : question })
+    question = get_object_or_404(Question, pk=question_id) 
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
