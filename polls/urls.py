54ba622617a56db1ff4fb69d5118cff005f9d192
Samuel P Holmes <samuel.holmes@cgius.com>
2019-02-26 10:50:00 -0600

    added employee

--- polls/urls.py
+++ polls/urls.py
@@ -4,10 +4,12 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+
     #ex: /1/
     path('<int:employee_id>/', views.detailEmployee, name='detailEmployee'), 
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
